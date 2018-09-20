ad258b7486bd57b36a7bab2c02989b94330ecc87
Qvita <42555696+Qvita@users.noreply.github.com>
2018-09-20 20:03:25 -0300

    Add files via upload

--- ej1_poo.py
+++ ej1_poo.py
@@ -1,9 +1,10 @@
 from persona import Persona
-#from juego1 import *
+#from combate import *
 
-var_nombre = input("\n Nombre? ")
-var_apellido = input("Apellido? ")
-var_edad = input("Edad?" )
+var_nombre = input("\n Apodo? ")
+var_edad = input(" Edad? " )
+
+var_n1 = input (" Nombre de tu rival? ")
 
 print("\n Por favor ingrese valores a las siguientes habilidades. Vida, Velocidad y fuerza. Recuerde que entre las 3 no puede sumar más de 20")
 
@@ -16,26 +17,77 @@ while (var_error == 0):
     else:
         print("\n Abajo de 20 crack")
 
-print("\n Quedan " , 20-var_vida , "puntos disponibles \n ")
+print("Quedan " , 20-var_vida , "puntos disponibles ")
 
 var_error = 2
 while (var_error == 2):
-    var_velocidad = int(input("\n Velocidad? "))
+    var_velocidad = int(input("Velocidad? "))
     if var_vida+var_velocidad < 21:
         var_error = 3
     else:
-        print("\n Abajo de 20 Total")
+        print("\n Abajo de 20 pero en total \n Quedan " , 20-var_vida , "puntos disponibles \n ")
 
-print("\n Quedan " , 20-var_vida-var_velocidad , "puntos disponibles")
+print("Quedan " , 20-var_vida-var_velocidad , "puntos disponibles")
 
 var_error = 4
 while (var_error == 4):
-    var_fuerza = int(input("\n Fuerza? "))
+    var_fuerza = int(input("Fuerza? \n"))
     if var_vida+var_velocidad+ var_fuerza < 21:
         var_error = 5
     else:
-        print("\n Bue...")
+        print("\n Bue... va de nuevo \n Quedan " , 20-var_vida-var_velocidad , "puntos disponibles")
 
-   
-p1 = Persona (var_nombre, var_apellido, var_edad, var_vida, var_velocidad, var_fuerza)
+var_clase = int(input("Ahora elije una clase, selecciona con 1, 2 o 3. \n 1. Peronista \n 2. Radical \n 3. Ultra Derecha \n "))
+
+if (var_clase == 1):
+    var_vida = var_vida + 10
+    print("\nEres Peronista, como van a durar para siempre obtienes 10 puntos extra de vida")
+elif (var_clase == 2):
+    var_velocidad = var_velocidad + 10
+    print("\nTienes la velocidad de un radical para hacer alianza con cualquiera \n Sumaste 10 puntos de velocidad")
+elif (var_clase == 3 ):
+    var_fuerza = var_fuerza + 10
+    print("\nElejiste Ultra derecha y obtienes +10 en fuerza bruta como es lógico")
+else:
+        print("Las opciones son 1, 2 o 3. \n")
+
+
+p1 = Persona (var_nombre, var_edad, var_vida, var_velocidad, var_fuerza )
 print(p1.saludar())
+
+
+def crear_personaje(nombre = "Jugador", ataque = 10, salud = 10):
+#esto construye un diccionario y lo retorna
+    personaje={
+        "nombre": nombre,
+        "ataque": ataque,
+        "salud": salud
+    }
+    return personaje
+def atacar(atacante, atacado):
+
+    #atacado["salud"]-=atacante["poder"]
+    recibir_ataque (atacado, atacante["ataque"])
+
+def recibir_ataque (personaje, ataque):
+    personaje["salud"]-= ataque 
+def probar():
+
+    p1=crear_personaje(nombre = var_nombre, ataque = var_fuerza, salud = var_vida)
+    p2=crear_personaje(nombre = var_n1, ataque = 15 , salud = 15)
+    print("Oh no!", var_nombre , " se la agito a" , var_n1)
+    print(p2)
+    atacar(p1,p2)
+    print(var_n1 , "Quedó medio juguete \n" , p2)
+    if(p2["salud"] < 0 ):
+        print(p2["nombre"],  "cago fuego")
+    else:
+        print("Y ahora que de que te disfrazas?\n" ,p1)
+    atacar(p2,p1)
+    if (p1["salud"] < 0 ):
+        print (p1["nombre"],  "La quedo")
+    else: 
+        print("que pelea aburrida")
+
+if __name__=="__main__":
+    probar()
--- persona.py
+++ persona.py
@@ -1,26 +1,26 @@
 
 class Persona:
     nombre = None
-    apellido = None
     edad = None
     vida = None
     velocidad = None
     fuerza = None
 
-    def __init__ (self, nombre, apellido, edad, vida, velocidad, fuerza):
+    def __init__ (self, nombre, edad, vida, velocidad, fuerza):
         self.nombre = nombre
-        self.apellido = apellido
         self.edad = edad
         self.vida = vida
         self.velocidad = velocidad
         self.fuerza = fuerza
 
     def saludar(self):
-        plantilla = "Hola, mi nombre es {nombre} {apellido}, "
-        plantilla += "mi edad es {edad}."
+        plantilla = "\n Bienvenido {nombre}, yo soy la interfaz"
+        plantilla += "\n Tus características son \n Vida {vida} \n Velocidad {velocidad} \n Fuerza {fuerza}. \n"
         saludo = plantilla.format(
             nombre = self.nombre,
-            apellido = self.apellido,
-            edad = self.edad
+            edad = self.edad,
+            vida = self.vida,
+            velocidad = self.velocidad,
+            fuerza = self.fuerza
         ) 
         return saludo
